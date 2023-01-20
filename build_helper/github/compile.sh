d2c36595ca83e7bf0d8646ff24b4058b6018693c
Lakshay Rohila <rohilarajnibala@gmail.com>
2023-01-20 16:30:06 +0530

    half commit

--- build_helper/github/compile.sh
+++ build_helper/github/compile.sh
@@ -6,10 +6,10 @@
 HELPER_DIR="./build_helper/github"
 BUILD_DIR_PRE="./ehdd-v$1"
 
-CC="gcc src/main.c -Wall -Wextra -Werror -static"
-
+CC_SUFFIX="src/main.c -Wall -Wextra -Werror $CFLAGS $LDFLAGS"
+#        . "/opt/x-tools/$CURR_COMP/activate"
 compile() {
-	$CC -m$1 -o $BUILD_DIR_PRE-$2/usr/bin/ehdd
+	$1 $CC_SUFFIX -o $BUILD_DIR_PRE-`cut -d'-' -f1 <<< $1`/usr/bin/ehdd
 
 	if [ $? -ne 0 ]; then
 		echo "Compilation failed: gcc returned with error"
@@ -17,5 +17,5 @@ compile() {
 	fi
 }
 
-compile "64" "x86_64"
-compile "32" "x86"
+compile "x86_64" "64" "x86_64"
+compile "i386" "32" "x86"
---
+++ build_helper/github/setup_toolchains.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# |--------------------------------------------------------------------------
+# | Compilers have been received from https://github.com/dimkr/toolchains.  |
+# | Thanks to the developers of toolchains for the software!                |
+# |--------------------------------------------------------------------------
+
+# This script setups the compilers to be used received from toolchains.
+
+# setup compilers to be used
+
+echo "Setting up compilers to be used ....."
+
+# GitHub REST API used. Read https://docs.github.com/rest/actions/artifacts
+# for more information on downloading artifacts from GitHub.
+LATEST_API_URL="https://api.github.com/repos/dimkr/toolchains/releases/latest"
+
+LATEST_TOOLCHAINS_TAG=$(curl -s "$LATEST_API_URL" | grep "tag_name.*" | cut -d: -f2 | cut -d\" -f2)
+
+# 'TOOLCHAINS_PACKED_SAVE_DIR' will be cached on GitHub workflow runs.
+# Get the cached dirs if present, otherwise create new.
+TOOLCHAINS_PACKED_SAVE_DIR="../toolchains_packed"
+EHDD_DIR="$(dirname "$(readlink -f "$0")")"
+
+if [ ! -d $TOOLCHAINS_PACKED_SAVE_DIR ]; then
+	mkdir $TOOLCHAINS_PACKED_SAVE_DIR
+	echo "#### NEW FILE ####" > "./toolchains_tag.metadata"
+fi
+
+cd "$TOOLCHAINS_PACKED_SAVE_DIR"
+
+if [ "$LATEST_TOOLCHAINS_TAG" != `cat "./toolchains_tag.metadata"` ]; then
+	DOWNLOADS_URLS=$(curl -s "$LATEST_API_URL" | grep "browser_download_url.*tar.gz" | cut -d: -f2,3 | tr -d \")
+
+	wget $DOWNLOADS_URLS || { printf "wget failed: Please read above errors.\nExiting!\n"; exit 1; }
+
+	echo "$LATEST_TOOLCHAINS_TAG" > "./toolchains_tag.metadata"
+fi
+
+for tar_pkg in *.tar.gz; do
+	[ ! -f "$tar_pkg" ] && { printf "No *.tar.gz found.\nExiting\n"; exit 1; }
+
+	CURR_COMP=$(echo "$tar_pkg" | cut -d. -f1)
+	COMPILERS_LIST="$COMPILERS_LIST$CURR_COMP\n"
+
+	sudo tar -xzvf "$tar_pkg" -C /
+done
+
+printf "$COMPILERS_LIST" > "./compilers_list.metadata"
+
+cd $EHDD_DIR
