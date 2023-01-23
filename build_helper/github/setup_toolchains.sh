49cca93b5cc1b31078472100afde0380db40effb
Lakshay Rohila <103126231+lakshayrohila@users.noreply.github.com>
2023-01-23 14:15:15 +0530

    Update setup_toolchains.sh

--- build_helper/github/setup_toolchains.sh
+++ build_helper/github/setup_toolchains.sh
@@ -6,26 +6,37 @@
 # |--------------------------------------------------------------------------
 
 # This script setups the compilers to be used received from toolchains.
-
-echo "Setting up compilers to be used ....."
+#
+# The code itself is split into three:-
+#   1. Code to check for available update ('-c' cmd line option)
+#   2. Code to download available update ('-d' cmd line option) -- ran only
+#      when '-c' returns with code '1'.
+#   3. Code to install toolchains ('-i' cmd line option)
 
 # GitHub REST API is used to read JSON data for latest toolchains release.
 LATEST_API_URL="https://api.github.com/repos/dimkr/toolchains/releases/latest"
-
-LATEST_TOOLCHAINS_TAG="$(curl -s "$LATEST_API_URL" | grep "tag_name" | cut -d: -f2 | cut -d\" -f2)"
 
 # 'TOOLCHAINS_PACKED_SAVE_DIR' will be cached on GitHub workflow runs.
 # Get the cached dirs if present, otherwise create new.
 TOOLCHAINS_PACKED_SAVE_DIR="../toolchains_packed"
 EHDD_DIR="$(dirname "$(readlink -f "$0")")"
 
+LATEST_TOOLCHAINS_TAG="$(curl -s "$LATEST_API_URL" | grep "tag_name" | cut -d: -f2 | cut -d\" -f2)"
+
 cd "$TOOLCHAINS_PACKED_SAVE_DIR"
 
-if [ ! -f ./toolchains_tag.metadata ]; then
-	echo 'NEW FILE' > ./toolchains_tag.metadata
-fi
+if [ $1 == '-c' ]; then
+	if [ ! -f ./toolchains_tag.metadata ]; then
+		echo 'NEW FILE' > ./toolchains_tag.metadata
+		exit 1
+	fi
 
-if [ "$LATEST_TOOLCHAINS_TAG" != "$(cat ./toolchains_tag.metadata)" ]; then
+	if [ "$LATEST_TOOLCHAINS_TAG" != "$(cat ./toolchains_tag.metadata)" ]; then
+		exit 1
+	fi
+
+	exit 0
+elif [ $1 == '-d' ]; then
 	TC_DOWNLOADS_URLS="$(curl -s "$LATEST_API_URL" | grep "browser_download_url" | cut -d: -f2,3 | tr -d \")"
 
 	echo "wget -t 3 $TC_DOWNLOADS_URLS"
@@ -33,17 +44,17 @@ if [ "$LATEST_TOOLCHAINS_TAG" != "$(cat ./toolchains_tag.metadata)" ]; then
 	wget -t 3 $TC_DOWNLOADS_URLS || { printf "wget failed: Please read above errors.\nExiting!\n"; exit 1; }
 
 	echo "$LATEST_TOOLCHAINS_TAG" > ./toolchains_tag.metadata
+elif [ $1 == '-i' ]; then
+	for tar_pkg in *.tar.gz; do
+		[ ! -f "$tar_pkg" ] && { printf "No *.tar.gz found.\nExiting\n"; exit 1; }
+
+		CURR_COMP=$(echo "$tar_pkg" | cut -d. -f1)
+		COMPILERS_LIST="$COMPILERS_LIST$CURR_COMP\n"
+
+		sudo tar -xzf "$tar_pkg" -C /
+	done
+
+	printf "$COMPILERS_LIST" > "./compilers_list.metadata"
 fi
-
-for tar_pkg in *.tar.gz; do
-	[ ! -f "$tar_pkg" ] && { printf "No *.tar.gz found.\nExiting\n"; exit 1; }
-
-	CURR_COMP=$(echo "$tar_pkg" | cut -d. -f1)
-	COMPILERS_LIST="$COMPILERS_LIST$CURR_COMP\n"
-
-	sudo tar -xzvf "$tar_pkg" -C /
-done
-
-printf "$COMPILERS_LIST" > "./compilers_list.metadata"
 
 cd $EHDD_DIR
