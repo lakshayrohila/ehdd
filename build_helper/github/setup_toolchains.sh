f20aa04830783a72e06ed86cefe5f264402ef3ef
Lakshay Rohila <rohilarajnibala@gmail.com>
2023-01-23 15:31:41 +0530

    HELLO

--- build_helper/github/setup_toolchains.sh
+++ build_helper/github/setup_toolchains.sh
@@ -27,7 +27,7 @@ cd "$TOOLCHAINS_PACKED_SAVE_DIR"
 
 case $1 in
 -c)
-	[ -f ./toolchains_tag.metadata ] || { echo 'NEW FILE' > ./toolchains_tag.metadata }
+	[ -f ./toolchains_tag.metadata ] || echo 'NEW FILE' > ./toolchains_tag.metadata
 
 	if [ "$LATEST_TOOLCHAINS_TAG" != "$(cat ./toolchains_tag.metadata)" ]; then
 		rm -rf ./*
